7340437ffc9bb9b18176d49eaf58a367296415f0
Lina <lina.murady@gmail.com>
2017-03-02 21:02:09 +0100

    functie om de verschillende 'speeches' te maken

--- textToSpeech/tts.py
+++ textToSpeech/tts.py
@@ -1,8 +1,21 @@
 from gtts import gTTS
 import os
 
-text = "Hallo douwe, hoeveel koeien zie jij?"
-language = "nl"
-tts = gTTS(text=text, lang=language)
-tts.save("speech.mp3")
-os.system("mpg123 speech.mp3")
+
+def generateSpeech(sentence, name):
+	language = "nl"
+	tts = gTTS(text=sentence, lang=language)
+	name = name + ".mp3"
+	tts.save(name)
+	os.system("mpg123 " + name)
+
+
+if __name__ == '__main__':
+	# Greet the user 
+	generateSpeech("Hallo! Leuk dat je er bent. Vandaag gaan wij lekker rekenen!", "begin")
+	# Correct answer 
+	generateSpeech("Dat is goed!", "goed")
+	# Wrong answer 
+	generateSpeech("Dat is fout!", "fout")
+	# Goodbye 
+	generateSpeech("Goed gewerkt vandaag! Tot de volgende keer", "dag")
